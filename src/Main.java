51ce253827512e85f9e33220a79730b179aa9c19
MossoMm <moskvin-07@list.ru>
2023-10-31 13:50:12 +0700

    3 d tochka

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome! ");
+
 
         String s = "Text";
         Point point = new Point(0,4);
