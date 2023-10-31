8069661ffb7da88219baadd203d3059d4e882155
MossoMm <moskvin-07@list.ru>
2023-10-31 13:50:54 +0700

    3 d tochka

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String s = "Text";
+
         Point point = new Point(0,4);
         System.out.println(point);
         Point point2 = new Point(3,0);
